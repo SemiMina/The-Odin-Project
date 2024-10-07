f6712d2128b38eb8fbd5ca2fbac748ac33fe7b80
Marina Kharkova <63921182+SemiMina@users.noreply.github.com>
2024-10-07 19:26:44 +0200

    Try variables and numbers

---
+++ js-foundations/javascript.js
@@ -0,0 +1,34 @@
+// Variables and Numbers
+
+// Variables
+const nm = "John";
+const surname = "Doe";
+let age = 11;
+
+age = 54;
+// TypeError: Assignment to constant variable.
+// nm = "Joe";
+
+console.log(nm, surname, age); // John Doe 54
+
+// Numbers
+let num = (3 + 2) - 76 * (1 + 1);
+console.log(num);
+
+num = 23 + 97;
+console.log(num);
+
+num = (4 + 6 + 9) / 77;
+console.log(num);
+
+console.log(4, 6, 9);
+
+num = 10;
+console.log(num);
+
+num = num * 9;
+console.log(num);
+
+let max = 57;
+let actual = max - 13;
+let perc = actual / max;
