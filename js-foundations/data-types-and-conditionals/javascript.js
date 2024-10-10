4117d406595f6cbee4b37d1f0c4d2771cf82d08c
Marina Kharkova <63921182+SemiMina@users.noreply.github.com>
2024-10-10 18:53:33 +0200

    Try comparisons

--- js-foundations/data-types-and-conditionals/javascript.js
+++ js-foundations/data-types-and-conditionals/javascript.js
@@ -94,3 +94,95 @@ console.log(str_1.at(0)); // L, the first character is in position 0
 console.log(str_1.at(-2)); // e
 
 console.log(str_1.length); // 5
+
+/*----------Comparisons----------*/
+let res = 'A' < 'B'; // true
+
+console.log(res);
+
+res = 'A' < 'a'; 
+
+console.log(res); // true, Unicode
+
+res = '2' > 1; 
+
+console.log(res); // true
+
+res = '2' > '12'; 
+
+console.log(res); // true
+
+res = '001' == 1; 
+
+console.log(res); // true
+
+console.log(Boolean("0")); // true
+
+console.log(Boolean(0)); // false
+
+// == converts to numbers
+console.log(0 == false); // true
+
+console.log('' == false); // true
+
+console.log('' == 0); // true
+
+// strict equality - without type conversion
+console.log(0 === false); // false
+
+console.log('' === false); // false
+
+console.log('' === 0); // false
+
+// comparison with null and undefined
+console.log(null == undefined); //true, they equal each other and don’t equal anything else
+
+console.log(null === undefined); // false
+
+// other comparisons convert null and undefined to numbers
+console.log(Number(null)); // 0
+
+console.log(Number(undefined)); // NaN
+
+console.log(null > undefined); // false
+
+console.log(null > 0); // false
+
+console.log(null == 0); // false
+
+console.log(null >= 0); // true, 0 >= 0
+
+// undefined is incomparable
+console.log(undefined > 0); // false, NaN > 0
+
+console.log(undefined == 0); // false
+
+console.log(undefined >= 0); // false
+
+console.log(undefined === 0); // false
+// NaN returns false for all comparisons
+
+/*----------Conditionals----------*/
+// else if
+let currentTime = 21;
+let greetingText;
+
+if (currentTime < 10) {
+    greetingText = "Good morning";
+} else if (currentTime < 20) {
+    greetingText = "Good day";
+} else {
+    greetingText = "Good evening";
+}
+
+console.log(greetingText);
+
+// random link
+let text;
+if (Math.random() < 0.5) {
+  text = "<a href='https://w3schools.com'>Visit W3Schools</a>";
+} else {
+  text = "<a href='https://wwf.org'>Visit WWF</a>";
+}
+
+console.log(text);
