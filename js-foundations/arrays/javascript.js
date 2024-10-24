f15f53a791f12fae580897269c015fc216fc82a7
Marina Kharkova <63921182+SemiMina@users.noreply.github.com>
2024-10-24 10:11:24 +0200

    Add the common array methods

--- js-foundations/arrays/javascript.js
+++ js-foundations/arrays/javascript.js
@@ -1,5 +1,3 @@
-debugger;
-
 // not a constant array! a constant referense to an array
 const skooters = [
     "Honda", 
@@ -75,5 +73,51 @@ let shifted_element = skooters.shift();
 new_length = skooters.unshift("Skoot"); 
 
 // Merging arrays (concatenating end-to-end)
+const myCats = ['Kuzya', 'Shoti'];
+const myDogs = ['Ichi'];
 
+const myPets = myCats.concat(myDogs);
+// (3) ['Kuzya', 'Shoti', 'Ichi']
 
+// concat method create a new array and can take many arguments
+// arrays as arguments
+// arr4 =  arr1.concat(arr2, arr3);
+// or strings as arguments
+// const myPets = myCats.concat('Ichi');
+
+debugger;
+// copy elements to another position in an array
+// it replaces elements
+const original_fruits = ["Banana", "Orange", "Apple", "Mango"];
+let fruits = original_fruits; // ! creates a connection between them!
+let new_fruits = fruits.copyWithin(2, 0);
+//(4) ['Banana', 'Orange', 'Banana', 'Orange']
+
+// flattering into a one-dimensional array
+const myArr = [[1,2],[3,4],[5,6]];
+const newArr = myArr.flat(); //(6) [1, 2, 3, 4, 5, 6]
+
+// splice() adds new elements and alters the original array
+fruits = original_fruits;
+
+fruits.splice(2, 0, "Lemon", "Kiwi");
+// The first parameter (2) defines the position where new elements should be added (spliced in).
+// The second parameter (0) defines how many elements should be removed.
+// The rest of the parameters ("Lemon" , "Kiwi") define the new elements to be added.
+
+// how to create a copy of an array
+const original_list_of_fruits = ["Banana", "Orange", "Apple", "Mango"];
+let list_of_fruits = original_list_of_fruits.slice(); // 
+
+// toSplice() keeps the original array unchanged and creates a new array
+let new_list = list_of_fruits.toSpliced(2, 0, "Lemon", "Kiwi");
+
+// slice() method slices out a piece of an array into a new array
+// The slice() method creates a new array.
+// The slice() method does not remove any elements from the source array.
+// starting from i slice(i)
+// starting from i up to j not including slice(i, j)
+const arrOfNum = [0, 1, 2, 3, 4, 5];
+const arrFrom0 = arrOfNum.slice();
+const arrFrom3 = arrOfNum.slice(3);
+const arrFrom2UpTo4 = arrOfNum.slice(2, 5);
