83b93f1759f4daf50a0623b142659a3f9b5274bf
Marina Kharkova <63921182+SemiMina@users.noreply.github.com>
2024-11-02 11:36:09 +0100

    Add UI and displaying results

--- js-foundations/rock-paper-scissors/javascript.js
+++ js-foundations/rock-paper-scissors/javascript.js
@@ -1,4 +1,77 @@
-function playGame() {
+/********************** Variables *********************/
+const btn_rock = document.getElementById("Rock");
+const btn_paper = document.getElementById("Paper");
+const btn_scissors = document.getElementById("Scissors");
+
+const btn_finish = document.getElementById("finish");
+const btn_clear = document.getElementById("clear");
+
+let compScore = 0;
+let humanScore = 0;
+
+let compTotalScore = 0;
+let humanTotalScore = 0;
+
+/******************** Events *********************/
+btn_rock.addEventListener("click", () => {
+    playRound("Rock");
+});
+
+btn_paper.addEventListener("click", () => {
+    playRound("Paper");
+});
+
+btn_scissors.addEventListener("click", () => {
+    playRound("Scissors");
+});
+
+btn_finish.addEventListener("click", () => {
+    if (compTotalScore + humanTotalScore === 0) {
+        logResult("Choose your weapon!");
+    } else {
+        getGameResult();
+        clearGameResult();
+    }
+});
+
+btn_clear.addEventListener("click", () => {
+    clearLog();
+    clearGameResult();
+});
+
+/******************** Results *********************/
+function getGameResult() {
+    if (compTotalScore > humanTotalScore) {
+        logResult('Game result: You lose!\nComputer score: ' + compTotalScore + '.\nYour score: ' + humanTotalScore + '.');
+    } else if (compTotalScore < humanTotalScore) {
+        logResult('Game result: You win!\nComputer score: ' + compTotalScore + '.\nYour score: ' + humanTotalScore + '.');
+    } else {
+        logResult('Game result: Draw!\nComputer score: ' + compTotalScore + '.\nYour score: ' + humanTotalScore + '.');
+    }
+}
+
+function clearGameResult() {
+    compTotalScore = 0;
+    humanTotalScore = 0;
+}
+
+/******************** Logs *********************/
+function logResult(logText) {
+    const res = document.querySelector(".result");
+    const p = document.createElement("p");
+    p.textContent += logText;
+    res.appendChild(p);
+}
+
+function clearLog() {
+    const res = document.querySelector(".result");
+    while (res.hasChildNodes()) {
+        res.removeChild(res.firstChild);
+    }
+}
+
+/******************** Rounds *********************/
+function playRound(humanChoice) {
 
     function getComputerChoice() {
         let randInt = Math.floor(Math.random() * 3); // 0/1/2
@@ -12,89 +85,51 @@ function playGame() {
         }
     }
 
-    function getHumanChoice() {
-        let choice = prompt(
-            'What is your choice? (Print "Rock" or "Paper" or "Scissors").'
-        );
+    compScore = 0;
+    humanScore = 0;
 
-        return capitalizeWord(choice);
-    }
+    let compChoice = getComputerChoice();
 
-    function capitalizeWord(word) {
-        return word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
-    }
-
-    let compScore = 0;
-    let humanScore = 0;
-
-    let compTotalScore = 0;
-    let humanTotalScore = 0;
-
-    let compChoice;
-    let humanChoice;
-
-    function playRound(compChoice, humanChoice) {
-        compScore = 0;
-        humanScore = 0;
-
-        compChoice = getComputerChoice();
-        humanChoice = getHumanChoice();
-
-        if (compChoice === "Rock") {
-            if (humanChoice === "Paper") {
-                humanScore += 2;
-            } else if (humanChoice === "Scissors") {
-                compScore += 2;
-            } else {
-                compScore += 1;
-                humanScore += 1;
-            }
-        } else if (compChoice === "Paper") {
-            if (humanChoice === "Rock") {
-                compScore += 2;
-            } else if (humanChoice === "Scissors") {
-                humanScore += 2;
-            } else {
-                compScore += 1;
-                humanScore += 1;
-            }
+    if (compChoice === "Rock") {
+        if (humanChoice === "Paper") {
+            humanScore += 2;
+        } else if (humanChoice === "Scissors") {
+            compScore += 2;
         } else {
-            if (humanChoice === "Rock") {
-                humanScore += 2;
-            } else if (humanChoice === "Paper") {
-                compScore += 2;
-            } else {
-                compScore += 1;
-                humanScore += 1;
-            }
+            compScore += 1;
+            humanScore += 1;
         }
-
-        if (compScore > humanScore) {
-            console.log('You lose! ' + compChoice + ' beats ' + humanChoice + '.');
-        } else if (compScore < humanScore) {
-            console.log('You win! ' + humanChoice + ' beats ' + compChoice + '.');
+    } else if (compChoice === "Paper") {
+        if (humanChoice === "Rock") {
+            compScore += 2;
+        } else if (humanChoice === "Scissors") {
+            humanScore += 2;
         } else {
-            console.log('Draw! ' + compChoice + ' VS ' + humanChoice + '.');
+            compScore += 1;
+            humanScore += 1;
         }
-
-        compTotalScore += compScore;
-        humanTotalScore += humanScore;
-    }
-
-    // play 5 rounds
-    playRound(compChoice, humanChoice);
-    playRound(compChoice, humanChoice);
-    playRound(compChoice, humanChoice);
-    playRound(compChoice, humanChoice);
-    playRound(compChoice, humanChoice);
-
-    if (compTotalScore > humanTotalScore) {
-        console.log('Game result: You lose! Computer score: ' + compTotalScore + '. Your score: ' + humanTotalScore + '.');
-    } else if (compTotalScore < humanTotalScore) {
-        console.log('Game result: You win! Computer score: ' + compTotalScore + '. Your score: ' + humanTotalScore + '.');
     } else {
-        console.log('Game result: Draw! Computer score: ' + compTotalScore + '. Your score: ' + humanTotalScore + '.');
+        if (humanChoice === "Rock") {
+            humanScore += 2;
+        } else if (humanChoice === "Paper") {
+            compScore += 2;
+        } else {
+            compScore += 1;
+            humanScore += 1;
+        }
     }
+
+    if (compScore > humanScore) {
+        logResult('You lose!\n' + compChoice + ' beats ' + humanChoice + '.');
+    } else if (compScore < humanScore) {
+        logResult('You win!\n' + humanChoice + ' beats ' + compChoice + '.');
+    } else {
+        logResult('Draw!\n' + compChoice + ' VS ' + humanChoice + '.');
+    }
+
+    compTotalScore += compScore;
+    humanTotalScore += humanScore;
 }
 
-playGame();
+
+
